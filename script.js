985ad06afacbee0a1b52c60c1754384bf5c0a0f0
suavepm <suavepm@icloud.com>
2022-10-23 11:49:13 -0700

    Restructured code to return resultMsg rather than prompting and alerting.

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 function getComputerChoice() {
+
     let randNum = Math.floor((Math.random() * 3));
     let handOutput;
     if (randNum === 0) {
@@ -9,29 +10,32 @@ function getComputerChoice() {
         handOutput = 'Scissors';
     }
     return handOutput;
+
 }
 
 function playRound(playerSelection, computerSelection) {
-    playerSelection = prompt('Rock, Paper, or Scissors?');
+
     playerSelection = playerSelection.toLowerCase();
     computerSelection = getComputerChoice().toLowerCase();
+    let resultMsg;
+
     if (playerSelection === 'rock' && computerSelection === 'paper') {
-        return alert('You lose! Paper beats Rock.');
+        resultMsg = 'You lose! Paper beats Rock.';
     } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
-        return alert('You win! Rock beats Scissors.');
+        resultMsg = 'You win! Rock beats Scissors.';
     } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
-        return alert('You lose! Paper beats Scissors.');
+        resultMsg = 'You lose! Paper beats Scissors.';
     } else if (playerSelection === 'paper' && computerSelection === 'rock') {
-        return alert('You win! Paper beats Rock.');
+        resultMsg = 'You win! Paper beats Rock.';
     } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
-        return alert('You lose! Rock beats Scissors.');
+        resultMsg = 'You lose! Rock beats Scissors.';
     } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
-        return alert('You win! Scissors beats Paper.');
+        resultMsg = 'You win! Scissors beats Paper.';
     } else if (playerSelection === computerSelection) {
-        return alert('Draw!');
+        resultMsg = 'Draw!';
     } else {
-        return alert('Invalid selection!');
+        resultMsg = 'Invalid selection!';
     }
+    return console.log(resultMsg);
 }
 
-playRound();
